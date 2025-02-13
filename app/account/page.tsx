77bb34192b7769baa517b9ab110ddf7f0b2711c2
Menno Drescher <menno@cbaconsult.eu>
2025-02-13 15:55:34 +0100

    Login and Account References and Referrals

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,6 +1,5 @@
 import AccountForm from './account-form'
 import { createClient } from '@/lib/utils/supabase/server'
-import { redirect } from 'next/navigation'
 
 export default async function Account() {
   const supabase = await createClient()
@@ -8,9 +7,6 @@ export default async function Account() {
   const {
     data: { user },
   } = await supabase.auth.getUser()
-  if (error || !data?.user) {
-    redirect('/login')
-  }
 
   return <AccountForm user={user} />
 }
